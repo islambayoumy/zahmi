0b68ad460e771e19abbadf43a8afed091d9c4150
Islam Bayoumy <islam.bayomy@gmail.com>
2017-04-30 05:51:13 +0200

    about us

--- website/urls.py
+++ website/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', MainView.home, name='home'),    
+    url(r'^about$', MainView.about, name='about'),    
     
     url(r'^login/$', auth_views.login, '''{'template_name': 'accounts/login.html'},''', name='login'),
     url(r'^logout/$', auth_views.logout, '''{'template_name': 'accounts/logout.html'},''', name='logout'),
--- website/views/MainView.py
+++ website/views/MainView.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'website/home.html')
 
+def about(request):
+    return render(request, 'website/about_us.html')
+
 
